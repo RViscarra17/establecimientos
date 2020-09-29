1f189e710adb956a25f68603c1f941664b4ad06f
VM17015 <VM17015@ues.edu.sv>
2020-09-28 22:43:34 -0600

    Creacion de Seed para Usuario y Categoria

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(UsuarioSeeder::class);
+        $this->call(CategoriaSeeder::class);
     }
 }
